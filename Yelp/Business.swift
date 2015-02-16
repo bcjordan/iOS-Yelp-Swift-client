7b76ec4c843bc336f71a48b1028dde26a019aa40
Brian Jordan <bcjordan@gmail.com>
2015-02-15 20:18:04 -0800

    Add other types of filters

--- Yelp/Business.swift
+++ Yelp/Business.swift
@@ -23,16 +23,25 @@ class Business {
         self.ratingImageUrl = fromDictionary["rating_img_url"] as? String
         self.numReviews = fromDictionary["review_count"] as? Int
 
-        var categoryArray = fromDictionary["categories"] as? NSArray
-        var categoriesStrings: [String] = []
-        for category in categoryArray! {
-            categoriesStrings.append(category[0] as String)
+        if let categoryArray = fromDictionary["categories"] as? NSArray {
+            var categoriesStrings: [String] = []
+            for category in categoryArray {
+                if (category.length > 0) {
+                    categoriesStrings.append(category[0] as String)
+                }
+            }
+            self.categories = ", ".join(categoriesStrings)
+        } else {
+            self.categories = ""
         }
-        self.categories = ", ".join(categoriesStrings)
         
-        var street: String = fromDictionary.valueForKeyPath("location.address")![0] as String
-        var neighborhood: String = fromDictionary.valueForKeyPath("location.neighborhoods")![0] as String
-        self.address = "\(street), \(neighborhood)"
+        if let streetArray: AnyObject = fromDictionary.valueForKeyPath("location.address") {
+            var street: String = fromDictionary.valueForKeyPath("location.address")![0] as String
+            var neighborhood: String = fromDictionary.valueForKeyPath("location.neighborhoods")![0] as String
+            self.address = "\(street), \(neighborhood)"
+        } else {
+            self.address = ""
+        }
         
         var metersPerMile: CGFloat = 0.000621371
 
--- Yelp/FiltersViewController.swift
+++ Yelp/FiltersViewController.swift
@@ -17,8 +17,32 @@ class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     var delegate: FiltersViewControllerDelegate?
     var filters: NSDictionary? = NSDictionary()
+    
     var categories: [[String: String]]?
+    var sorts: [[String: String]] = [
+        ["name": "Best Match", "code": "sort", "value": "0"],
+        ["name": "Distance", "code": "sort", "value": "1"],
+        ["name": "Highest Rated", "code": "sort", "value": "2"]
+    ]
+    var distances: [[String: String]] = [
+        ["name": "Half mile", "code": "radius_filter", "value": "804.67200"],
+        ["name": "1 mile", "code": "radius_filter", "value": "1609.344"],
+        ["name": "2 miles", "code": "radius_filter", "value": "3218.688"]
+    ]
+    var deals: [[String: String]] = [
+        ["name": "Deals only", "code": "deals_filter", "value": "true"]
+    ]
+    
     var selectedCategories: NSMutableSet?
+    var selectedSorts: NSMutableSet?
+    var selectedDistances: NSMutableSet?
+    var selectedDeals: NSMutableSet?
+    
+    let nameCategories = "Categories"
+    let nameSort = "Sort"
+    let nameDistance = "Distance"
+    let nameDeals = "Deals"
+    let sectionNames = ["Categories", "Sort", "Distance", "Deals"]
     
     func getFilters() -> [String: String] {
         var filters = [String: String]()
@@ -32,6 +56,30 @@ class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewD
             var categoryFilter: String = ",".join(names)
             filters["category_filter"] = categoryFilter
         }
+        
+        if (self.selectedSorts!.count >= 0) {
+            for item in selectedSorts! {
+                let value = item["value"] as NSString
+                let key = item["code"] as NSString
+                filters[key] = "\(value)"
+            }
+        }
+        
+        if (self.selectedDistances!.count >= 0) {
+            for item in selectedDistances! {
+                let value = item["value"]  as NSString
+                let key = item["code"] as NSString
+                filters[key] = "\(value)"
+            }
+        }
+        
+        if (self.selectedDeals!.count >= 0) {
+            for item in selectedDeals! {
+                let value = item["value"]  as NSString
+                let key = item["code"] as NSString
+                filters[key] = "\(value)"
+            }
+        }
         return filters
     }
     
@@ -39,6 +87,9 @@ class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewD
         super.viewDidLoad()
         
         self.selectedCategories = NSMutableSet()
+        self.selectedSorts = NSMutableSet()
+        self.selectedDistances = NSMutableSet()
+        self.selectedDeals = NSMutableSet()
         
         self.initCategories()
     }
@@ -360,10 +411,33 @@ class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewD
     
     func switchCell(cell: SwitchTableViewCell, updatedValue: Bool) {
         let indexPath = self.tableView.indexPathForCell(cell)!
-        if (updatedValue) {
-            self.selectedCategories!.addObject(self.categories![indexPath.row])
-        } else {
-            self.selectedCategories!.removeObject(self.categories![indexPath.row])
+        switch (sectionNames[indexPath.section]) {
+        case nameCategories:
+            if (updatedValue) {
+                self.selectedCategories!.addObject(self.categories![indexPath.row])
+            } else {
+                self.selectedCategories!.removeObject(self.categories![indexPath.row])
+            }
+        case nameSort:
+            if (updatedValue) {
+                self.selectedSorts!.addObject(self.sorts[indexPath.row])
+            } else {
+                self.selectedSorts!.removeObject(self.sorts[indexPath.row])
+            }
+        case nameDistance:
+            if (updatedValue) {
+                self.selectedDistances!.addObject(self.distances[indexPath.row])
+            } else {
+                self.selectedDistances!.removeObject(self.distances[indexPath.row])
+            }
+        case nameDeals:
+            if (updatedValue) {
+                self.selectedDeals!.addObject(self.deals[indexPath.row])
+            } else {
+                self.selectedDeals!.removeObject(self.deals[indexPath.row])
+            }
+        default:
+            print("Default")
         }
     }
     
@@ -372,20 +446,46 @@ class FiltersViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let switchCell: SwitchTableViewCell = tableView.dequeueReusableCellWithIdentifier("switchcell") as SwitchTableViewCell
         switchCell.delegate = self
-        switchCell.setOn(self.selectedCategories!.containsObject(self.categories![indexPath.row]))
-        switchCell.titleLabel.text = self.categories![indexPath.row]["name"]
+        switch (sectionNames[indexPath.section]) {
+        case nameCategories:
+            switchCell.setOn(self.selectedCategories!.containsObject(self.categories![indexPath.row]))
+            switchCell.titleLabel.text = self.categories![indexPath.row]["name"]
+        case nameSort:
+            switchCell.setOn(self.selectedSorts!.containsObject(self.sorts[indexPath.row]))
+            switchCell.titleLabel.text = self.sorts[indexPath.row]["name"]
+        case nameDistance:
+            switchCell.setOn(self.selectedDistances!.containsObject(self.distances[indexPath.row]))
+            switchCell.titleLabel.text = self.distances[indexPath.row]["name"]
+        case nameDeals:
+            switchCell.setOn(self.selectedDeals!.containsObject(self.deals[indexPath.row]))
+            switchCell.titleLabel.text = self.deals[indexPath.row]["name"]
+        default:
+            return switchCell
+        }
         return switchCell
     }
     
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        if (section == 0) {
+        switch (sectionNames[section]) {
+        case nameCategories:
             return self.categories!.count
+        case nameSort:
+            return self.sorts.count
+        case nameDistance:
+            return self.distances.count
+        case nameDeals:
+            return self.deals.count
+        default:
+            return 0
         }
-        return 0
     }
     
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return sectionNames.count
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return sectionNames[section]
     }
 
 }
